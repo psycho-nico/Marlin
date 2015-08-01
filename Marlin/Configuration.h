83578c2ef72312122b895d369f40b78b5ca0b1db
Philippe Vanhaesendonck <AmedeeBulle@users.noreply.github.com>
2015-08-01 17:49:08 +0200

    Externalize MAXTEMP

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -193,8 +193,12 @@
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!
 // You should use MINTEMP for thermistor short/failure protection.
-#define HEATER_0_MAXTEMP 275
-#define HEATER_1_MAXTEMP 275
+#ifndef HEATER_0_MAXTEMP
+  #define HEATER_0_MAXTEMP 275
+#endif
+#ifndef HEATER_1_MAXTEMP
+  #define HEATER_1_MAXTEMP 275
+#endif
 #define HEATER_2_MAXTEMP 275
 #define BED_MAXTEMP 150
 
